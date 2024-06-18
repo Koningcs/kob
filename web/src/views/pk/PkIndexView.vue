4c3c6057d638d8a617689b397c47474995f5dd9c
LuckyDog <790956212@qq.com>
2024-06-18 22:37:01 +0800

    对战页面实现游戏地图

--- web/src/views/pk/PkIndexView.vue
+++ web/src/views/pk/PkIndexView.vue
@@ -1,9 +1,16 @@
 <template>
+    <PlayGround> </PlayGround>
     <div>对战</div>
 </template>
 
 <script>
+import PlayGround from '../../components/PlayGround.vue'
 
+export default {
+    components: {
+        PlayGround
+    }
+}
 </script>
 
 <style scoped> 
